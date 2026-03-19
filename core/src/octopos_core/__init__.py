2fa6d02025b474b22cffe56631bd2c47bbcae80f
tilleulenspiegel <thebit@rocketmail.com>
2026-03-19 23:24:31 +0100

    feat(core): update __init__.py with runtime exports

--- core/src/octopos_core/__init__.py
+++ core/src/octopos_core/__init__.py
@@ -1,4 +1,5 @@
 from .agent_config import AgentConfig, load_agent_config
 from .agent_discovery import AgentDiscovery
+from .agent_runtime import AgentRuntime, AgentStatus
 
-__all__ = ["AgentConfig", "load_agent_config", "AgentDiscovery"]
+__all__ = ["AgentConfig", "load_agent_config", "AgentDiscovery", "AgentRuntime", "AgentStatus"]
