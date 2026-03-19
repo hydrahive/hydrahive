7078e8a27289e6b6dd3c7ff82d3a5db254504e09
tilleulenspiegel <thebit@rocketmail.com>
2026-03-19 23:42:23 +0100

    feat(core): update __init__.py with session exports

--- core/src/octopos_core/__init__.py
+++ core/src/octopos_core/__init__.py
@@ -1,13 +1,14 @@
 from .agent_config import AgentConfig, load_agent_config
 from .agent_discovery import AgentDiscovery
 from .agent_runtime import AgentRuntime, AgentStatus
-from .project_config import ProjectConfig, load_project_config
+from .project_config import ProjectConfig
 from .project_loader import ProjectLoader
+from .session_manager import Session, SessionManager, Message, MessageRole
 
 __all__ = [
     "AgentConfig", "load_agent_config",
     "AgentDiscovery",
     "AgentRuntime", "AgentStatus",
-    "ProjectConfig", "load_project_config",
-    "ProjectLoader",
+    "ProjectConfig", "ProjectLoader",
+    "Session", "SessionManager", "Message", "MessageRole",
 ]
