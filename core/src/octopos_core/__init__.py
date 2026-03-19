79f4ddaa3b2c96d0b13bc7887d36725c35556fee
tilleulenspiegel <thebit@rocketmail.com>
2026-03-19 23:36:38 +0100

    feat: ProjectConfig + ProjectLoader in __init__ exportieren (#7)

--- core/src/octopos_core/__init__.py
+++ core/src/octopos_core/__init__.py
@@ -1,5 +1,13 @@
 from .agent_config import AgentConfig, load_agent_config
 from .agent_discovery import AgentDiscovery
 from .agent_runtime import AgentRuntime, AgentStatus
+from .project_config import ProjectConfig, load_project_config
+from .project_loader import ProjectLoader
 
-__all__ = ["AgentConfig", "load_agent_config", "AgentDiscovery", "AgentRuntime", "AgentStatus"]
+__all__ = [
+    "AgentConfig", "load_agent_config",
+    "AgentDiscovery",
+    "AgentRuntime", "AgentStatus",
+    "ProjectConfig", "load_project_config",
+    "ProjectLoader",
+]
