b40b1fbfa5a6d2d7a41ddc344c068125cd006b3e
tilleulenspiegel <thebit@rocketmail.com>
2026-03-19 23:49:10 +0100

    feat(core): update __init__.py with orchestrator exports

--- core/src/octopos_core/__init__.py
+++ core/src/octopos_core/__init__.py
@@ -4,6 +4,9 @@ from .agent_runtime import AgentRuntime, AgentStatus
 from .project_config import ProjectConfig
 from .project_loader import ProjectLoader
 from .session_manager import Session, SessionManager, Message, MessageRole
+from .tool_registry import BaseTool, ToolRegistry, registry
+from .skill_loader import Skill, load_skills, select_skills
+from .orchestrator import Orchestrator
 
 __all__ = [
     "AgentConfig", "load_agent_config",
@@ -11,4 +14,7 @@ __all__ = [
     "AgentRuntime", "AgentStatus",
     "ProjectConfig", "ProjectLoader",
     "Session", "SessionManager", "Message", "MessageRole",
+    "BaseTool", "ToolRegistry", "registry",
+    "Skill", "load_skills", "select_skills",
+    "Orchestrator",
 ]
